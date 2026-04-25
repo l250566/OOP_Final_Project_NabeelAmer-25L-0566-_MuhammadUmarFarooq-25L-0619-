ab219f87b8c8f9426aa8bc988c4cef737573a3f3
l250566 <l250566@lhr.nu.edu.pk>
2026-04-25 16:30:01 +0500

    Added set and get virtual functions

--- src/Node.h
+++ src/Node.h
@@ -24,6 +24,10 @@ public:
     virtual void create() = 0;
     virtual void open() = 0;
     virtual void del() = 0;
+    virtual void setname(string nname) = 0;
+    virtual void setnode(node *&parent) = 0;
+    virtual string getname() = 0;
+    virtual node *&getnode() = 0;
 };
 
 #endif
